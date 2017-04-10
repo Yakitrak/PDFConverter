1451aed2dfd9fc1620fbefd46d42739987999a1d
Yakitrak <kartikayjainwal@gmail.com>
2017-04-10 19:12:59 +0100

    Add files via upload

---
+++ PDFconverter.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import glob
+import win32com.client
+
+
+def main():
+    ifConvert = input("Enter [YES] to convert files or anything else to exit: ")
+    if (ifConvert.upper() == "YES"):
+        callConvert()
+    else:
+        exit()
+
+def callConvert():
+    name = input("What is the file location to convert from: ")
+    convFileType = input("Would you like to convert [PPT] or [DOC] or [BOTH]: ")
+    if (convFileType.upper() == "PPT"):
+        files = glob.glob(name + "\*.ppt*")
+        convertPPT(files)
+    elif (convFileType.upper() == "DOC"):
+        files = glob.glob(name + "\*.doc*")
+        convertDOC(files)
+    elif (convFileType.upper() == "BOTH"):
+        files = glob.glob(name + "\*.ppt*")
+        convertPPT(files)
+        files = glob.glob(name + "\*.doc*")
+        convertDOC(files)
+    else:
+        print("Sorry, that is not one of the options. Please try again")
+    main()
+
+def convertPPT(files, formatType=32):
+    powerpoint = win32com.client.Dispatch("Powerpoint.Application")
+    powerpoint.Visible = 1
+    print(files)
+    for filename in files:
+        newname = os.path.splitext(filename)[0] + ".pdf"
+        file = powerpoint.Presentations.Open(filename)
+        file.SaveAs(newname, formatType)
+        file.Close()
+    powerpoint.Quit()
+
+def convertDOC(files, formatType=17):
+    word = win32com.client.Dispatch("Word.Application")
+    word.Visible = 1
+    print(files)
+    for filename in files:
+        newname = os.path.splitext(filename)[0] + ".pdf"
+        file = word.Documents.Open(filename)
+        file.SaveAs(newname, formatType)
+        file.Close()
+    word.Quit()
+
+main()
+
